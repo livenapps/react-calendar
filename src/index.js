2def01cd3b9744d66a48d987c867aa3fa59aad23
Your Name <you@example.com>
2019-11-07 22:58:28 +0300

    Add markup and styles to year view

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Calendar from './components/Calendar';
 import * as serviceWorker from './serviceWorker';
+const date = '02.29.2020'; // month.day.year // not required
 
-ReactDOM.render(<Calendar date={new Date()}/>, document.getElementById('react-calendar'));
+ReactDOM.render(
+    <Calendar
+        date={date}
+        onChangeDay={''}
+        onChangeMonth={''}
+        onChangeYear={''}
+        onChange={''}
+    />,
+    document.getElementById('react-calendar'));
 
 serviceWorker.unregister();
