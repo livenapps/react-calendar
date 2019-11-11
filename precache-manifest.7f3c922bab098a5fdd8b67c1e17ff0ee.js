8f7123754f3ac8e19f2691b357c12f3690e0e793
m.kulikova <m.kulikova@itransition.com>
2019-11-11 14:25:10 +0300

    Updates

---
+++ precache-manifest.7f3c922bab098a5fdd8b67c1e17ff0ee.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "047e2ea383c4c870132afc3d0d25b9de",
+    "url": "/react-calendar/index.html"
+  },
+  {
+    "revision": "6d072ee94aaa23c3e51c",
+    "url": "/react-calendar/static/css/main.9e9e2044.chunk.css"
+  },
+  {
+    "revision": "06d1dabb2a9f41dd8c1d",
+    "url": "/react-calendar/static/js/2.519722e2.chunk.js"
+  },
+  {
+    "revision": "6d072ee94aaa23c3e51c",
+    "url": "/react-calendar/static/js/main.d8162360.chunk.js"
+  },
+  {
+    "revision": "e3d8bb547d6ce18b73ac",
+    "url": "/react-calendar/static/js/runtime-main.88d22331.js"
+  },
+  {
+    "revision": "cf82dccccff1190189b8523a22caf851",
+    "url": "/react-calendar/static/media/arrow-bottom.cf82dccc.svg"
+  },
+  {
+    "revision": "875e6086755f2497fa4f2e9cb90a640d",
+    "url": "/react-calendar/static/media/arrow-left.875e6086.svg"
+  },
+  {
+    "revision": "2fa3a8445104f81ec7854e901a9efdaf",
+    "url": "/react-calendar/static/media/arrow-right.2fa3a844.svg"
+  },
+  {
+    "revision": "74831de2253a316c65e58750ef89440f",
+    "url": "/react-calendar/static/media/arrow-top.74831de2.svg"
+  },
+  {
+    "revision": "391652eaa4b5c7d12d413cf259ac5494",
+    "url": "/react-calendar/static/media/tile-menu.391652ea.svg"
+  }
+]);
